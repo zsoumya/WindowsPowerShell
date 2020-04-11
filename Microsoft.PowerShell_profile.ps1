c4c607b65e7b0451b237be3cfb116bb0da2cf1f8
zsoumya <zsoumya@yahoo.com>
2020-04-11 19:37:35 -0400

    Updated prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,23 @@
 ﻿Import-Module posh-git -RequiredVersion 1.0.0
 
-$promptChar = 'ᐅ'
+$promptChar = '⚡'
+$promptPathForeColor = 'magenta'
+$promptPrefixForeColor = 'yellow'
+$promptStringForeColor = 'darkred'
+
 $newLine = [System.Environment]::NewLine
 $bit = @{ $True = 'x64'; $False = 'x86' }[[System.Environment]::Is64BitProcess]
 $label = if ($PSVersionTable.PSEdition -eq "Core") { "PWSH" } else { "PS" }
 $promptString = ($promptChar * ($nestedPromptLevel + 1)) + ' '
 
-$Global:GitPromptSettings.DefaultPromptPath.ForegroundColor = 'magenta'
+$Global:GitPromptSettings.DefaultPromptPath.ForegroundColor = $promptPathForeColor
+$Global:GitPromptSettings.DefaultPromptPath.Text = '$(if ((Get-PromptPath) -eq $HOME) { "$(Get-PromptPath)(~)" } else { Get-PromptPath })'
+
+$Global:GitPromptSettings.DefaultPromptSuffix.ForegroundColor = $promptStringForeColor
 $Global:GitPromptSettings.DefaultPromptSuffix.Text = "$newLine$promptString"
-$Global:GitPromptSettings.DefaultPromptSuffix.ForegroundColor = 'green'
-$Global:GitPromptSettings.DefaultPromptPrefix.Text = "$label$bit "
-$Global:GitPromptSettings.DefaultPromptPrefix.ForegroundColor = 'yellow'
+
+$Global:GitPromptSettings.DefaultPromptPrefix.ForegroundColor = $promptPrefixForeColor
+$Global:GitPromptSettings.DefaultPromptPrefix.Text = "$label$bit·"
+
 $Global:GitPromptSettings.DefaultPromptWriteStatusFirst = $false
+$Global:GitPromptSettings.PathStatusSeparator.Text = "·"
