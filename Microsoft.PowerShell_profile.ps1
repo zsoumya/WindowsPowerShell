921f2d10c029fa617b4d9e7b0c783051004408a5
zsoumya <zsoumya@yahoo.com>
2020-04-11 12:00:06 -0400

    Added profile for VS Code

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ $label = if ($PSVersionTable.PSEdition -eq "Core") { "PWSH" } else { "PS" }
 $promptString = ('$' * ($nestedPromptLevel + 1)) + ' '
 
 $global:GitPromptSettings.DefaultPromptPath.ForegroundColor = 'magenta'
-$global:GitPromptSettings.DefaultPromptSuffix = "$newLine$ " * ($nestedPromptLevel + 1)
+$global:GitPromptSettings.DefaultPromptSuffix = "$newLine$promptString"
 $global:GitPromptSettings.DefaultPromptSuffix.ForegroundColor = 'green'
 $global:GitPromptSettings.BeforeStatus.ForegroundColor = 'darkyellow'
 $global:GitPromptSettings.AfterStatus.ForegroundColor = 'darkyellow'
